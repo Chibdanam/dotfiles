ed9da6d6cf7c2da9a78033c31f8c26f55c24a5ca
Mikaël COIN-MANSOGO ALO <mikael.coinmansogoalo@proton.me>
2026-06-23 21:28:20 +0000

    add git, zoxide, and eza shell aliases (lg, amend, undo, wip, cd, ls, ll, lt)

--- config/zsh/aliases.zsh
+++ config/zsh/aliases.zsh
@@ -8,3 +8,8 @@ alias bat='batcat'
 mkcd() { mkdir -p -- "$1" && cd -- "$1"; }
 alias vswap="rm -rf ~/.local/state/nvim/swap/"
 alias fetch="macchina"
+
+alias cd='z'
+alias ls='eza'
+alias ll='eza -l'
+alias lt='eza --tree --git-ignore --level=2'
